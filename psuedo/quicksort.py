8295bc405d77d988b56a863c73d0ef046705b9fc
coffeesnakes <coffeesnakes@gmail.com>
2023-10-23 19:23:51 -0700

    yeee

---
+++ psuedo/quicksort.py
@@ -0,0 +1,12 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr  # Base case: return if array has 1 or 0 elements
+    pivot = arr[len(arr) // 2]  # Choose the middle element as the pivot
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)  # Recursively sort and combine
+
+# Testing the function
+arr = [3, 6, 8, 10, 1, 2, 1]
+print(quicksort(arr))  # Output: [1, 1, 2, 3, 6, 8, 10]
