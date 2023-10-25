8232e021ba0f1dc5c3c0889ab339724352692293
coffeesnakes <coffeesnakees@gmail.com>
2023-10-24 22:53:29 -0700

    what am i doing

--- psuedo/getWeather.sh
+++ psuedo/getWeather.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
+# Get the location as a command line argument
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$location
+)
+
+# Check the weather using wttr.in
+$response = Invoke-WebRequest -Uri "http://wttr.in/$location"
+
+# Print the weather information
+$response.Content#!/bin/bash
 
 # Get the location as a command line argument
 location=$1
 
 # Check the weather using wttr.in
 curl "wttr.in/${location}"
+
+# chmod +x getWeather.sh // icacls .\getWeather.sh /grant Everyone:F
+
